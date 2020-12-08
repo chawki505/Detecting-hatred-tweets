ef341d6360ef9646421bf8592e6a57f0d6e54e9b
Chawki CHOUIB <chouibc@gmail.com>
2020-12-08 19:27:22 +0100

    fix normalisation to remove emoji

--- main.py
+++ main.py
@@ -48,6 +48,9 @@ def normalisation(text: str) -> str:
     # remove contraction
     text = contractions.fix(text)
 
+    # make str low
+    text = text.lower()
+
     # remove http and url
     url_reg = re.compile('http(s)?://\S+|www\.\S+')
     text = url_reg.sub(r'', text)
@@ -59,17 +62,6 @@ def normalisation(text: str) -> str:
     # remove new line characters
     text = text.replace('\n', '')
 
-    # remove Emojis
-    emoji_reg = re.compile("["u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
-
-    text = emoji_reg.sub(r'', text)
-
     # remove stopwords
     text = ' '.join([elem for elem in text.split() if elem not in mystopwords])
 
@@ -79,6 +71,11 @@ def normalisation(text: str) -> str:
     # remove user
     text = text.replace("user", "")
 
+    # remove Emojis
+    emoji_reg = re.compile("[^A-Za-z ]+")
+
+    text = emoji_reg.sub(r'', text)
+
     return text
 
 
@@ -86,12 +83,12 @@ def create_set(path):
     set_x = []
     set_y = []
     for line in open(path, "r").readlines():
-        elem = line.split(",")
+        elem = line.split(",", maxsplit=2)
 
         set_x.append(normalisation(elem[2]))
         set_y.append(elem[1])
 
-        return set_x, set_y
+    return set_x, set_y
 
 
 if __name__ == '__main__':
